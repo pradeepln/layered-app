0dc1e3be71fcaee6abaf0cea9a65abdb6cdd41c5
Pradeep LN <pradeep.ln78@gmail.com>
2021-03-01 11:57:00 +0530

    starting code for discussing Spring Boot

---
+++ src/com/training/ProductApplication.java
@@ -0,0 +1,14 @@
+package com.training;
+
+import com.training.ui.ProductConsoleUI;
+
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		
+		ProductConsoleUI ui = new ProductConsoleUI();
+		ui.createProductWithUI();
+
+	}
+
+}
